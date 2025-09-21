2660742eb0b071b09f7637b38a1a07d28269fcfb
minhquan <quanleminh2310@gmail.com>
2025-09-21 22:48:56 +0700

    Add WebMVC folder & customer-management project

---
+++ WebMVC/customer-management/src/main/java/exam/codegym/customermanagement/service/CustomerService.java
@@ -0,0 +1,17 @@
+package exam.codegym.customermanagement.service;
+
+import exam.codegym.customermanagement.model.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void update(int id, Customer customer);
+
+    void remove(int id);
+}
